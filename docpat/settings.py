bf8b615e5d615781d10ad4bf2a38261c304e2a69
Pratham Agarwal <ansh1234agarwal@gmail.com>
2025-08-03 02:30:51 +0530

    Initial commit for Doc-Pat

--- docpat/settings.py
+++ docpat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ca%df(7xzy&&kpkz&+sgwatxzlzam7i$u8e%oaerrd^%-m(zs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["DOCPAT-SMART-DISEASE-PREDICTOR.onrender.com", "127.0.0.1"]
 
 
 # Application definition
